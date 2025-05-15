1eea866dcc660a450534f17d31032a166a1119be
ritam-sarkar <rit.style@gmail.com>
2025-05-15 10:31:52 +0530

    add more problems

---
+++ ProblemSolving/src/com/orcl/array/MergeInterval.java
@@ -0,0 +1,30 @@
+package com.orcl.array;
+
+import java.util.*;
+
+public class MergeInterval {
+    public static void main(String[] args) {
+        int[][] arr = {{2,3},{2,2},{3,3},{1,3},{5,7},{2,2},{4,6}};
+        merge(arr);
+        System.out.println(arr);
+    }
+    static int[][] merge(int[][] intervals) {
+        // Step 1: sort by start time
+        Arrays.sort(intervals, (a, b) -> Integer.compare(a[0], b[0]));
+
+        List<int[]> merged = new ArrayList<>();
+
+        for (int[] interval : intervals) {
+            // If merged list is empty OR no overlap
+            if (merged.isEmpty() || merged.get(merged.size() - 1)[1] < interval[0]) {
+                merged.add(interval);
+            } else {
+                // Overlap → merge
+                merged.get(merged.size() - 1)[1] = Math.max(merged.get(merged.size() - 1)[1], interval[1]);
+            }
+        }
+
+        return merged.toArray(new int[merged.size()][]);
+    }
+
+}
